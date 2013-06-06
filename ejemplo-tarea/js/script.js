2526106c43a3e50ba3ec241ce480e5b1c6a1a118
crowcpga <crowcpga@gmail.com>
2013-06-05 21:14:47 -0400

    creando la estructura

--- ejemplo-tarea/js/script.js
+++ ejemplo-tarea/js/script.js
@@ -1,6 +1,7 @@
 var App = {
 	startup: function(){
-		console.log( "startup: la pagina se cargo correctamente!" );	
+		//console.log( "startup: la pagina se cargo correctamente!" );	
+		
 	}
 }
 window.onload = App.startup;
