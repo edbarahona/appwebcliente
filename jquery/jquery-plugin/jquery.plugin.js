86237f4a025743f92aea4318abddf2074fafc04b
Carlos González Aedo <crowcpga@gmail.com>
2013-06-08 18:00:52 -0400

    cambiando el texto cuando hagan click

--- jquery/jquery-plugin/jquery.plugin.js
+++ jquery/jquery-plugin/jquery.plugin.js
@@ -1,9 +1,12 @@
 (function( $ ,w , undefined){
 	$.fn.HolaMundo = function(){
+		var msg = "Hola Mundo!";
+		//recorremos los elementos asociados al plugin
 		$( this ).each( function( index , li ){
-
-			var html = $( li ).html();
-			alert( html );
+			//por cada elemento le asocio que cuando me hagan click muestre el mensaje dentro del elemento.
+			$(li).on('click',function(){
+				$(this).text( msg );
+			});
 		});
 	}
 })(jQuery,window);
--- jquery/jquery-plugin/script.js
+++ jquery/jquery-plugin/script.js
@@ -1,3 +1,4 @@
 $( document ).on('ready',function(){
+	//relacinamos un elemento al plugin
 	$('li').HolaMundo();
 });
